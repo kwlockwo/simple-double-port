50a1f50a026400e5732ed02490617cd3791cb1f5
Keith Lockwood <kwlockwo@gmail.com>
2023-06-29 16:16:31 +1000

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const externaApp = express()
+const externalPort = process.env.PORT || 5000
+
+const internalApp = express()
+const internalPort = process.env.INTERNAL_PORT || 6000
+
+externaApp.get("/", (req, res) => {
+    res.send(`You have reached the external app on port=${externalPort}`)
+})
+
+internalApp.get("/", (req, res) => {
+    res.send(`You have reached the internal app on port=${internalPort}`)
+})
+
+externaApp.listen(externalPort, () => {
+    console.log(`External app listening on port ${externalPort}`)
+})
+
+internalApp.listen(internalPort, () => {
+    console.log(`Internal app listening on port ${internalPort}`)
+})
